c74f2830f9037c187e669c344598d9edb1713e6d
Alexander Mendoza <alexanderjosueph@gmail.com>
2022-12-03 12:51:02 -0400

    Modificaciones clase1 git status

--- Pruebas/1erPrueba.js
+++ Pruebas/1erPrueba.js
@@ -8,13 +8,21 @@
 // Los booleanos son maneras de validad información cómo TRUE o FALSE 
 true
 false
+// ==================================================================//
 
 // Los valores vacíos. 
 // Podemos usar null O undefined
 null
 undefined
 
+// ======================================================================//
 // Valores de tipo objeto
  [1,2,3,4]
 
  {nombre: "Diego"}
+
+
+ // =====================================================================//
+
+// Para conocer el tipo de dato en Javascript hay un comando llamado "typeof"
+// Este comando lo ejecutas en la consola del navegador seguido del valor y te da cómo resultado el tipo de valor que es. 
