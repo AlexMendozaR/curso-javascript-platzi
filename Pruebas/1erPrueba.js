896eaf54d600bea8743c32b11648fc4d8dbfc477
Alexander Mendoza <alexanderjosueph@gmail.com>
2022-12-03 12:30:03 -0400

    Cambios a clase 1

---
+++ Pruebas/1erPrueba.js
@@ -0,0 +1,20 @@
+// A estos valores se les conoce cómo valores PRIMITIVOS.
+
+// Para usar números lo podemos escribir directamente
+50 
+// Para crear un string lo debemos colocar entre comillas, un string es texto
+"Alexander Mendoza"
+
+// Los booleanos son maneras de validad información cómo TRUE o FALSE 
+true
+false
+
+// Los valores vacíos. 
+// Podemos usar null O undefined
+null
+undefined
+
+// Valores de tipo objeto
+ [1,2,3,4]
+
+ {nombre: "Diego"}
