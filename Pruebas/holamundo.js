fa634acddc5b09c07e098df0850cb6eecf620033
Alexander Mendoza <alexanderjosueph@gmail.com>
2022-12-06 16:05:41 -0400

    Agregado clases del libro

---
+++ Pruebas/holamundo.js
@@ -0,0 +1,22 @@
+console.log ("Hola mundo");
+console.log ("Bienvenidos al libro de aprender Javascript  ");
+let nombre = "Alexander";
+let apellido = "Mendoza Rodriguez";
+
+const pi = 3.1416;
+const velocidadLuz = 3000000;
+
+var Nombre = "Josueph";
+
+console.log ("Mensaje 1");
+console.log ("Mensaje 2");
+
+console.log (pi);
+console.log (Nombre);
+
+console.log (nombre , apellido);
+
+let ciudad = "Barquisimeto";
+let edad = "23";
+console.log ("Mi edad es %s años" , edad);
+console.log ("Mi nombre es %s y tengo %s años, vivo en %s", nombre, edad, ciudad);
---
+++ clase5.md
@@ -0,0 +1,8 @@
+# Clase 5 
+
+
+## Scope 
+
+Significa el alcance que tienen las variables.
+
+Tiene que ver con el hecho de donde inicies y declares una variable para saber sí tendrás acceso a ella. 
