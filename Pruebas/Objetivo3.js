702f5a9b45025ab213b7baff2f5a837521e1fab5
Alexander Mendoza <alexanderjosueph@gmail.com>
2022-12-07 23:03:20 -0400

    Agregada clase6

--- Pruebas/Objetivo3.js
+++ Pruebas/Objetivo3.js
@@ -1,4 +1,16 @@
-const prompt = require("prompt-sync")();
-let informacionLeida = prompt("Escribe tu nombre: ");
+// const prompt = require("prompt-sync")();
+// let informacionLeida = prompt("Escribe tu nombre: ");
 
-console.log("Hola", informacionLeida)
+// console.log("Hola", informacionLeida)
+
+var miNombre = "Alexander";
+var miApellido = "Mendoza";
+
+function func () {
+    var ciudad = "Barquisimeto";
+    console.log("Mi nombre es " + miNombre + " " + miApellido + " Y soy de " + ciudad);
+}
+
+func();
+
+// Para llamar a una funcion que estemos llamando siempre se debe colocar el nombre de la funcion que queremos ejecutar seguida de (); para que sea realizada. 
--- clase5.md
+++ clase5.md
@@ -6,3 +6,8 @@
 Significa el alcance que tienen las variables.
 
 Tiene que ver con el hecho de donde inicies y declares una variable para saber sí tendrás acceso a ella. 
+
+// Para llamar a una funcion que estemos llamando siempre se debe colocar el nombre de la funcion que queremos ejecutar seguida de (); para que sea realizada. 
+
+__Podemos definir el Scope de una variable cómo "Donde vive y donde muere" o "Su alcance".__
+
