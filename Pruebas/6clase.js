c3f9e3fd824a9d5f80bf8a2ca7e4cc252c60eee4
Alexander Mendoza <alexanderjosueph@gmail.com>
2022-12-08 23:07:28 -0400

    Clase7

---
+++ Pruebas/6clase.js
@@ -0,0 +1,8 @@
+var miNombre;
+miNombre = "Alexander";
+var miApellido = "Mendoza";
+var miCiudad = "Barquisimeto";
+var orlando = "Orlando, Florida, USA."
+
+
+console.log("Hola mi nombre es %s %s y soy de %s, en poco tiempo iré a %s", miNombre, miApellido, miCiudad, orlando);
