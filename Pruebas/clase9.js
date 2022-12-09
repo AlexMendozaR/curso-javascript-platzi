000ddf32f1d2b4ae9ce943e9a47d0ba67e1e8b4e
Alexander Mendoza <alexanderjosueph@gmail.com>
2022-12-09 11:37:42 -0400

    Agregada clase 8 y 9

---
+++ Pruebas/clase9.js
@@ -0,0 +1,30 @@
+// Operadores 
+
+2 + 2 
+50 - 10
+10 * 20
+20 / 2 
+
+// Estos son operadores que nos ayudan a hacer operaciones matemáticas y son conocidos cómo operadores Binarios. 
+
+"Alexander" + "Mendoza" // Sirve para concadenar(Los junta) los dos String (Sting es texto)
+
+!false // Con el carácter ! se niega lo que viene en siguiente, es decir en este caso estamos negando el "False" por lo tanto si no es falso será verdadero. 
+
+var a = 1; // Con el carácter de = le asignamos el valor a la variable. El igual nos ayuda a asignar valores. 
+
+3 == "3" // Sí usamos dos igual juntos (==) entonces estará comparando el valor, no asignando valores, hará la función de comparar.
+
+3 === "3" // Este estará comparando sí son el mismo tipo de valor y tienen el mismo typeof. En este caso sería (false) dado que 3 es un número y "3" es un string. 
+
+// Podemos usar los valores matematicos tal cómo < > <= =>
+
+6 < 3 
+5 > 2
+5 <= 6 
+6 >= 5
+
+a && b // Este dará TRUE si AMBOS valores son TRUE 
+
+a || b // Este dará TRUE si alguno de los valores es TRUE. 
+
